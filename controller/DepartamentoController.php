7e78fbfaba2993fe28104956acc7caf293fe04a0
Bilal El Kadi El Moussaoui <usrpract02@globales.es>
2026-04-09 14:00:03 +0200

    Modelo de Departamento terminado

---
+++ controller/DepartamentoController.php
@@ -0,0 +1,15 @@
+<?php
+
+include_once("models/Departamento.php");
+
+class DepartamentoController{
+    //método principal para mostar la lista
+    public function listar(){
+        //instanciamos el modelo
+        $depObj = new Departamento();
+    }
+}
+
+
+
+?>
--- model/Departamento.php
+++ model/Departamento.php
@@ -6,7 +6,28 @@ include_once("db.php");
 class Departamento {
     //guardamos la conexióna a la base de datos
     private $db;
-    private $tabla = "Departamentos";
+    private $nombre_tabla = "Departamentos";
+
+    //constructor
+    public function __construct(){
+        $this->db = Db::conectar();
+    }
+
+    //método para obtener datos
+    public function listar(){
+        //definimos la consula SQL
+        $sql = "SELECT * FROM " . $this->nombre_tabla;
+
+        //preparamos la consulta
+        $stmt = $this->db->prepare($sql);
+
+        //ejecutamos la consulta en la base de datos
+        $stmt->execute();
+
+        //fetchALL(PDO::ASSOC) convierte las filas de la tabla en un arreglo asociativo de PHP
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
 }
 
 
